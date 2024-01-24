e8b8b2515d877beeaa1a4d7105bc10db95d60bb3
Andsirpa <andsirpa94@gmail.com>
2024-01-24 20:14:12 +0100

    add grid functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log('JS OK');
+// recupero gli elementi HTML
+
+const gridContainer = document.getElementById("grid-container");
+const playButton = document.getElementById("play-button");
+const difficulty = document.getElementById('select-difficulty')
+const result = document.getElementById("result");
+
